f91055680841c162cbcee65b46130e6ee0611820
Atmani Chouaib <atmanichouaib22@gmail.com>
2024-01-14 14:53:03 +0100

    controllers/views for only Read pages

--- express-locallibrary-tutorial/models/author.js
+++ express-locallibrary-tutorial/models/author.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const { DateTime } = require("luxon") ;
 
 const Schema = mongoose.Schema;
 
@@ -25,6 +26,28 @@ AuthorSchema.virtual("name").get(function () {
 AuthorSchema.virtual("url").get(function () {
   // We don't use an arrow function as we'll need the this object
   return `/catalog/author/${this._id}`;
+});
+
+AuthorSchema.virtual("date_of_birth_formatted").get(function () {
+  return this.date_of_birth?
+  DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.Date_MED): "not Available"
+});
+
+AuthorSchema.virtual("date_of_death_formatted").get(function () {
+  return this.date_of_death?
+  DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.Date_MED): "Not available"
+});
+
+AuthorSchema.virtual("lifespan").get(function () {
+  let lifetime = ''
+  if(this.date_of_birth) {
+    lifetime += DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.Date_MED);
+  }
+  lifetime += ' - '
+  if(this.date_of_death) {
+    lifetime += DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.Date_MED);
+  }
+  return lifetime ;
 });
 
 // Export model
